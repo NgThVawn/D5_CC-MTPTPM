ee9e1d7af6f1622f8c916e61abadd11fcb8e5755
NgThVawn <nguyenthanhvane@gmail.com>
2025-10-28 12:29:32 +0700

    Khởi tạo dự án

---
+++ DoAnCS/WedBanHang/Models/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebBanHang.Models;
+
+namespace WedBanHang.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<ReviewImage> ReviewImages { get; set; }
+        public DbSet<Promotion> Promotions { get; set; }
+        public DbSet<PromotionProduct> PromotionProducts { get; set; }
+        public DbSet<PromotionCategory> PromotionCategories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<PromotionProduct>()
+                .HasKey(x => new { x.PromotionId, x.ProductId });
+
+            modelBuilder.Entity<PromotionCategory>()
+                .HasKey(x => new { x.PromotionId, x.CategoryId });
+        }
+
+
+    }
+}
